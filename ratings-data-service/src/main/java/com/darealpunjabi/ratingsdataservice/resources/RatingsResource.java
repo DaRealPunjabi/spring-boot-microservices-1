04f4d73afd494dd76c58e3413499e329dffb6be8
DaRealPunjabi <shiv.jalli@gmail.com>
2019-10-05 20:32:06 +0100

    08. Calling Ratings Data API

--- ratings-data-service/src/main/java/com/darealpunjabi/ratingsdataservice/resources/RatingsResource.java
+++ ratings-data-service/src/main/java/com/darealpunjabi/ratingsdataservice/resources/RatingsResource.java
@@ -2,6 +2,7 @@ package com.darealpunjabi.ratingsdataservice.resources;
 
 
 import com.darealpunjabi.ratingsdataservice.models.Rating;
+import com.darealpunjabi.ratingsdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,4 +15,13 @@ public class RatingsResource {
     public Rating getRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
     }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+
+    }
+
 }
